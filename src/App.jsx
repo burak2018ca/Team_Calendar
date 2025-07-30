5de2edf21d98c659bd7a70a775b3118a16ef73d8
Burak_Yildirim <burak2018ca@hotmail.com>
2025-07-30 11:58:44 -0400

    Project set Up

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <div className="div">
+        <h1>Team Calendar</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
